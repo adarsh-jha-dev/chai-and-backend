468c3dcdaefbc2239298010e61ee3f571cf1b43d
Adarsh Jha <adarshjhaxif@gmail.com>
2023-11-24 14:43:21 +0530

    Basic Utils done

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// THE TRY CATCH PATTERN
+
+// const asyncHandler = (fn) => async (req, res, next) =>{
+//     try {
+//         await fn(req, res, next);
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success : false,
+//             message : err.message,
+//         })
+//     }
+// }
