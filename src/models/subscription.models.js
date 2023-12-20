2dac641fa64062f6d4daf412dde379465a23ac65
Adarsh Jha <adarshjhaxif@gmail.com>
2023-12-20 08:15:02 +0530

    Added update user controllers

---
+++ src/models/subscription.models.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId, // one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: mongoose.Schema.Types.ObjectId, // the one who is being subscribed
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export default Subscription = mongoose.model(
+  "Subscription",
+  subscriptionSchema
+);
