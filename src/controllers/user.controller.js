34db2ad81303c6dfcc26b2080fc80dd4e3e41300
Adarsh Jha <adarshjhaxif@gmail.com>
2023-12-23 20:46:39 +0530

    Aggregation added for channel profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -366,6 +366,80 @@ const UpdateUserCoverImage = asyncHandler(async (req, res) => {
   }
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  try {
+    const { username } = req.params;
+    if (!username.trim()) {
+      throw new ApiError(400, "username is missing");
+    }
+
+    const channel = await User.aggregate([
+      {
+        $match: {
+          username: username?.toLowerCase(),
+        },
+      },
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "channel",
+          as: "subscribers",
+        },
+      },
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "subscriber",
+          as: "subscribedTo",
+        },
+      },
+      {
+        $addFields: {
+          subscribersCount: {
+            $size: "$subscribers",
+          },
+          channelSubscribedToCount: {
+            $size: "$subscribedTo",
+          },
+          isSubscribed: {
+            $cond: {
+              if: { $in: [req?.user?._id, "$subcribers.subscriber"] },
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          fullname: 1,
+          username: 1,
+          subscribersCount: 1,
+          channelSubscribedToCount: 1,
+          isSubscribed: 1,
+          avatar: 1,
+          coverImage: 1,
+          email: 2,
+        },
+      },
+    ]);
+
+    if (!channel?.length) {
+      throw new ApiError(404, "Channel doesn't exist");
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully")
+      );
+  } catch (error) {
+    console.log(error);
+    throw new ApiError(500, "Internal server errror");
+  }
+});
+
 export {
   registerUser,
   loginUser,
@@ -376,4 +450,5 @@ export {
   UpdateAccountDetails,
   UpdateUserAvatar,
   UpdateUserCoverImage,
+  getUserChannelProfile,
 };
